1db146c6ee34a08c86cf2b38c9c874f8b29a0838
Jordy <Jordy.tak@gmail.com>
2019-02-22 12:49:50 +0100

    test2

--- eftest/Controllers/HomeController.cs
+++ eftest/Controllers/HomeController.cs
@@ -57,40 +57,7 @@ namespace eftest.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-
-        public IActionResult oudelaptoptest()
-        {
-            string test = "dsfsdfsdfsdfdsf";
-            //v
-            //v
-            //v
-            //v
-            //v
-            //v
-            //v
-            //v
-
-            return View();
-        }
-
-
-
-        public IActionResult nieuwelaptoptest()
-        {
-            string test = "dit is een test";
-            //c
-            //c
-            //c
-            //c
-            //c
-            //c
-            //c
-            //c
-            //c
-
-            return View();
-        }
-
+       
 
     }
 }
